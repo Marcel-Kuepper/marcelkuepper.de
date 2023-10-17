a8fab5c2709ccfcfcf852636751b66d8783efca8
Marcel Kuepper <marcelk95@hotmail.de>
2023-10-17 15:06:03 +0200

    slideshow added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'marcelkuepper-app';
+export class AppComponent implements OnInit {
+  images = ['desktop.jpg', 'laptop.jpg', 'laptop2.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;
+      }, 5)
+    }, 8000);
+  }
+
 }
