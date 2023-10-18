b5cc035e025e7a06f9928290eb3c4b7a3acec3e3
Marcel Kuepper <marcelk95@hotmail.de>
2023-10-18 16:11:40 +0200

    slideshow text added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   images = ['desktop.jpg', 'laptop.jpg', 'laptop2.jpg'];
+  headlines = ['Bringing engineering to the next level!', 'Born to code', 'Graduated at DeveloperAkademie'];
   currentImage = 0;
   showImage = true;
 
