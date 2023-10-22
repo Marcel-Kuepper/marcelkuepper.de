ac5d843177b6667997524459a35ba462c1574af8
Marcel Kuepper <marcelk95@hotmail.de>
2023-10-22 23:16:17 +0200

    added headder menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,25 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  images = ['desktop.jpg', 'laptop.jpg', 'laptop2.jpg'];
-  headlines = ['Bringing engineering to the next level!', 'Born to code', 'Graduated at DeveloperAkademie'];
-  currentImage = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-      setTimeout(() => {
-        this.showImage = true;
-      }, 5)
-    }, 8000);
-  }
+export class AppComponent {
 
 }
---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+  images = ['desktop.jpg', 'laptop.jpg', 'laptop2.jpg'];
+  headlines = ['Bringing engineering to the next level!', 'Born to code', 'Graduated at DeveloperAkademie'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;
+      }, 5)
+    }, 8000);
+  }
+}
